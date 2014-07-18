ee667cc0477012ec64d351074bdd448ddae6168d
Stephan Zlatarev <stephan.zlatarev@sap.com>
2014-07-18 17:13:12 +0300

    candles.html calibrates the x-axis tick size of the chart

---
+++ src/test/resources/testdata.js
@@ -0,0 +1,6 @@
+data = [
+  { timestamp: 1405623125617, open: 135267, high: 135277, low: 135247, close: 135257 },
+  { timestamp: 1405625335617, open: 135367, high: 135377, low: 135347, close: 135357 },
+  { timestamp: 1405627545617, open: 135267, high: 135277, low: 135247, close: 135257 },
+  { timestamp: 1405629755617, open: 135267, high: 135177, low: 135147, close: 135157 },
+]
